4bb88601033273b4753c3ac9f69f01bfc12987e9
thomas-ring <thomasjring@icloud.com>
2017-12-12 00:01:26 -0500

    Added a bunch of working, non-mediocre code

--- Main.hs
+++ Main.hs
@@ -2,22 +2,22 @@ module Main where
 
 -- Import necessary dependecies
 import qualified Data.List as List
-import Data.Char
 import qualified Data.Map
+import Data.Char
 import System.Environment
 import System.IO
 
 -- Define a main function to read arguments and do something with them
-main :: IO ()
+main :: IO BFInterpreter
 main = do
   a <- getArgs
   inf <- openFile (head a) ReadMode
   process_file inf
   
-process_file :: Handle -> IO ()
+process_file :: Handle -> IO BFInterpreter
 process_file h = process_file_r [] h
 
-process_file_r :: [Char] -> Handle -> IO ()
+process_file_r :: [Char] -> Handle -> IO BFInterpreter
 process_file_r chars h = do
   ineof <- hIsEOF h
   if ineof
@@ -35,161 +35,201 @@ valid_char c = let valids = ['<', '>', '+', '-', '.', ',', '[', ']'] in
     Just _ -> True
     Nothing -> False
 
-interpret_program :: [Char] -> IO ()
-interpret_program c = let bfi = bfi_with_instrs c in
-  interpret_program_r bfi
+interpret_program :: [Char] -> IO BFInterpreter
+interpret_program c = interpret_program_r (bfi_io_wrap (bfi_init c))
 
-interpret_program_r :: BFInterpreter -> IO()
-interpret_program_r bfi
-  | bfi_completed bfi = return ()
-  | otherwise = let next_bfi = interpret_program_r (bfi_execute_next bfi) in
-      let op = (bfi_next_ip next_bfi) in
-        next_bfi
+interpret_program_r :: IO BFInterpreter -> IO BFInterpreter
+interpret_program_r io_bfi = do
+  bfi <- io_bfi
+  if bfi_completed bfi then
+    return bfi
+    else
+    if (bfi_next_code bfi) == '.' || (bfi_next_code bfi) == ',' then
+      do
+        interpret_program_r
+        -- Here we increment the code_ptr before we send it to perform
+        -- the operation because (1) the code pointer is not needed during
+        -- IO operations and (2) we can avoid another unwrap.
+          (case bfi_next_code bfi of
+             '.' -> bfi_output (bfi_inc_cp bfi)
+             ',' -> bfi_input (bfi_inc_cp bfi)
+             _ -> io_bfi)
+    else
+      interpret_program_r (bfi_io_wrap (bfi_inc_cp (
+            case bfi_next_code bfi of
+              '>' -> (bfi_inc_tp bfi)
+              '<' -> (bfi_dec_tp bfi)
+              '+' -> (bfi_inc_v bfi)
+              '-' -> (bfi_dec_v bfi)
+              '[' -> (bfi_jmp_f bfi)
+              ']' -> (bfi_jmp_b bfi)
+              -- Again, we ignore invalid input (bad practice...)
+              _ -> bfi
+            )))
 
--- =============================
--- === Upper level type defs ===
--- =============================
+bfi_output ::  BFInterpreter -> IO BFInterpreter
+bfi_output bfi = do
+--  putChar (bfi_get_v_char bfi)
+  putStr [(bfi_get_v_char bfi)]
+  return bfi
 
--- BFInterpreter is the actual machine that handles input, the
--- instruction pointer, and the execution of the code.
-data BFInterpreter = BF {tape :: Tape,
-                         instr_p :: Int,
-                         instrs :: [Char],
-                         bracket_map :: Data.Map.Map Int Int}
+bfi_input :: BFInterpreter -> IO BFInterpreter
+bfi_input bfi = do
+  inp <- getChar
+  return (bfi_set_v bfi (ord inp))
 
--- Returns a new, blank BFI
-new_bfi :: BFInterpreter
-new_bfi = BF blank_tape 0 [] Data.Map.empty
+bfi_io_wrap :: BFInterpreter -> IO BFInterpreter
+bfi_io_wrap bfi = do
+  return bfi
 
-bfi_with_instrs :: [Char] -> BFInterpreter
-bfi_with_instrs i = BF blank_tape 0 i Data.Map.empty
+-- Receives a single character from stdin and returns it as an IO monad
+--input_ch :: IO Char
 
--- Returns a BFI with a different tape
+-- Prints the passed in Char to stdout
+--output_ch :: Char -> IO ()
+
+-- ===========================================
+-- === BFInterpreter definitions/inteface  ===
+-- ===========================================
+
+-- BFInterpreter encapsulates all data needed to process a .bf
+-- file program.
 --
--- Useful for updating the tape after a calculation
-bfi_with_tape :: BFInterpreter -> Tape -> BFInterpreter
-bfi_with_tape bfi t = BF t (instr_p bfi) (instrs bfi)
-                      (bracket_map bfi)
+-- It is recommended to use all the convenience methods below to modify
+-- the values, as they will ensure that edge cases are taken care of.
+data BFInterpreter = BF {tape :: [Int],
+                         tape_ptr :: Int,
+                         code :: [Char],
+                         code_ptr :: Int,
+                         bracemap :: Data.Map.Map Int Int}
 
--- Executes the next step in the BFI's instrs set
-bfi_execute_next :: BFInterpreter -> BFInterpreter
-bfi_execute_next bfi = let res = bfi_execute_ch (bfi_cur_instr bfi) bfi in
-  BF (tape res) (bfi_next_ip bfi) (instrs bfi) (bracket_map bfi)
+-- Initializes a BFInterpreter, also creating a bracemap in the
+-- process. It is recommended to use this method.
+bfi_init :: [Char] -> BFInterpreter
+bfi_init c = let b = bracemap_for_code c in
+  BF [0] 0 c 0 b
 
--- Executes the single char using the BFI
-bfi_execute_ch :: Char -> BFInterpreter -> BFInterpreter
-bfi_execute_ch c bfi
-  | c == '>' || c == '<' || c == '+' || c == '-' =
-      bfi_with_tape bfi (tape_execute_ch c $! (tape bfi))
-  | c == '.' || c == ',' =
-    let t = tape_execute_io c (tape bfi) in
-      bfi_with_tape bfi t
+-- Convenience methods
+
+bfi_completed :: BFInterpreter -> Bool
+bfi_completed bfi = (code_ptr bfi) >= (length (code bfi))
+
+bfi_next_code :: BFInterpreter -> Char
+bfi_next_code bfi = ((code bfi) !! (code_ptr bfi))
+
+bfi_inc_cp :: BFInterpreter -> BFInterpreter
+bfi_inc_cp bfi = bfi_with_code_ptr bfi ((code_ptr bfi) + 1)
+
+bfi_get_v :: BFInterpreter -> Int
+bfi_get_v bfi = (tape bfi) !! (tape_ptr bfi)
+
+bfi_get_v_char :: BFInterpreter -> Char
+bfi_get_v_char bfi = Data.Char.chr (bfi_get_v bfi)
+
+-- Sets the input value at the tape_ptr
+bfi_set_v :: BFInterpreter -> Int -> BFInterpreter
+bfi_set_v bfi inp = bfi_with_tape bfi (
+  (take (tape_ptr bfi) (tape bfi) ++ [inp] ++
+   (drop ((tape_ptr bfi) + 1) (tape bfi))))
+
+-- Common overwriting methods
+
+bfi_with_tape :: BFInterpreter -> [Int] -> BFInterpreter
+bfi_with_tape bfi t = BF t (tape_ptr bfi) (code bfi) (code_ptr bfi)
+  (bracemap bfi)
+
+bfi_with_tape_ptr :: BFInterpreter -> Int -> BFInterpreter
+bfi_with_tape_ptr bfi t_p = BF (tape bfi) t_p (code bfi) (code_ptr bfi)
+  (bracemap bfi)
+
+bfi_with_code_ptr :: BFInterpreter -> Int -> BFInterpreter
+bfi_with_code_ptr bfi c_p = BF (tape bfi) (tape_ptr bfi) (code bfi)
+  c_p (bracemap bfi)
+
+
+-- =========================
+-- === Operation Methods ===
+-- =========================
+
+-- '>'
+-- Increments the tape ptr one value.
+bfi_inc_tp :: BFInterpreter -> BFInterpreter
+bfi_inc_tp bfi = if ((tape_ptr bfi) + 1) >= (length (tape bfi))
+                 then
+                   bfi_with_tape
+                   (bfi_with_tape_ptr bfi ((tape_ptr bfi) + 1))
+                   ((tape bfi) ++ [0])
+                   else
+                   bfi_with_tape_ptr bfi ((tape_ptr bfi) + 1)
+
+-- '<'
+-- Decrements the tape ptr one value.
+bfi_dec_tp :: BFInterpreter -> BFInterpreter
+bfi_dec_tp bfi = if ((tape_ptr bfi) == 0)
+                 then
+                   bfi_with_tape bfi ([0] ++ (tape bfi))
+                   else
+                   bfi_with_tape_ptr bfi ((tape_ptr bfi) - 1)
+
+-- '+'
+-- Increments the value at tape_ptr one value.
+bfi_inc_v :: BFInterpreter -> BFInterpreter
+bfi_inc_v bfi = bfi_set_v bfi (
+  if (bfi_get_v bfi) < 255 then
+    ((bfi_get_v bfi) + 1)
+    else
+      0
+  )
+  
+
+-- '-'
+-- Decrements the value at tape_ptr one value.
+bfi_dec_v :: BFInterpreter -> BFInterpreter
+bfi_dec_v bfi = bfi_set_v bfi (
+  if (bfi_get_v bfi) > 0 then
+    ((bfi_get_v bfi) - 1)
+    else
+      255
+  )
+
+-- '['
+-- Moves the code_ptr forward if current value is 0
+bfi_jmp_f :: BFInterpreter -> BFInterpreter
+bfi_jmp_f bfi
+  | (bfi_get_v bfi) == 0 =
+    bfi_with_code_ptr bfi ((bracemap bfi)
+                            Data.Map.! (code_ptr bfi))
   | otherwise = bfi
 
--- Executes a single char using the tape and it's convenience methods
-tape_execute_ch :: Char -> Tape -> Tape
-tape_execute_ch c t = t `seq`
-                      if c == '>' then increment_dp t
-                      else if c == '<' then decrement_dp t
-                      else if c == '+' then increase_value t
-                      else if c == '-' then decrease_value t
-                      else t
+-- ']'
+-- Moves the code_ptr backwards if current value is not 0
+bfi_jmp_b :: BFInterpreter -> BFInterpreter
+bfi_jmp_b bfi
+  | (bfi_get_v bfi) /= 0 =
+    bfi_with_code_ptr bfi ((bracemap bfi)
+                            Data.Map.! (code_ptr bfi))
+  | otherwise = bfi
 
-tape_execute_io :: Char -> Tape -> IO Tape
-tape_execute_io c t = if c == '.' then output_dp t
-                      else if c == ',' then input_dp t
-                           else return t
+-- Returns the 'bracemap' object used by the BFInterpreter given
+-- the list of inputs.
+bracemap_for_code :: [Char] -> Data.Map.Map Int Int
+bracemap_for_code c =
+  bracemap_r 0 c [] Data.Map.empty
 
-bfi_next_ip :: BFInterpreter -> Int
-bfi_next_ip bfi = bfi_next_ip_for_ch (bfi_cur_instr bfi) bfi
-
--- Returns the next instruction pointer given the BFI
-bfi_next_ip_for_ch :: Char -> BFInterpreter -> Int
-bfi_next_ip_for_ch c bfi
-  | c == '[' || c == ']' = let m = (bracket_map bfi) in
-      if (cur_value (tape bfi)) == 0 then
-        (Data.Map.findWithDefault (instr_p bfi) (instr_p bfi) m) + 1
-      else
-        (instr_p bfi) + 1
-  | otherwise = (instr_p bfi) + 1
-
--- Returns the current instruction
-bfi_cur_instr :: BFInterpreter -> Char
-bfi_cur_instr bfi = (instrs bfi) !! (instr_p bfi)
-
--- Returns True if the interpreter is finished with it's instructions,
--- False otherwise.
-bfi_completed :: BFInterpreter -> Bool
-bfi_completed bfi = (length (instrs bfi)) <= (instr_p bfi)
-
--- =======================
--- === Tape Definition ===
--- =======================
-
--- Tape handles the data and the pointer to the data.
-data Tape = Tp {data_p :: Int,
-                tape_list :: [Int]}
-
--- A default constructor for a Tape.
-blank_tape :: Tape
-blank_tape = Tp 0 [0]
-
-cur_value :: Tape -> Int
-cur_value t = (tape_list t) !! (data_p t)
-
--- A convenience method to get the character that is being pointed
--- to currently.
-char_at_head :: Tape -> Char
-char_at_head t = Data.Char.chr ((tape_list t) !! (data_p t))
-
--- ========================
--- === Mapped functions ===
--- ========================
-
--- '>' operator
--- Increment data point
-increment_dp :: Tape -> Tape
-increment_dp t
-  | length (tape_list t) == data_p t =
-    Tp (1 + data_p t) ((tape_list t) ++ [0])
-  | otherwise = Tp (1 + data_p t) (tape_list t)
-
--- '<' operator
--- Decrement data point
-decrement_dp :: Tape -> Tape
-decrement_dp t
-  | 0 == data_p t = Tp (data_p t) ([0] ++ (tape_list t))
-  | otherwise = Tp (data_p t - 1) (tape_list t)
-
--- '+' operator
--- Increase value at dp by one byte
-increase_value :: Tape -> Tape
-increase_value t = Tp (data_p t)
-  (let (s, e) = List.splitAt (data_p t) (tape_list t) in
-     s ++ [(head e + 1)] ++ (tail e))
-
--- '-' operator
--- Decrement value at dp by one byte
-decrease_value :: Tape -> Tape
-decrease_value t = let p = (data_p t) in
-  Tp p ((List.take (p - 1) (tape_list t)) ++
-        [(((tape_list t) !! p) - 1)] ++
-        (List.drop (p + 1) (tape_list t)))
-
--- '.' operator
--- Output thebyte at the data pointer
-output_dp :: Tape -> IO Tape
-output_dp t = do
-  putStrLn "Putting a char!"
-  putChar (char_at_head t)
-  return $! t
-
--- ',' operator
--- Accept one byte of input, storing its value in the byte
--- at the dp.
-input_dp :: Tape -> IO Tape
-input_dp t = do c <- getChar
-                let v = ord c in
-                  let (s, e) = List.splitAt (data_p t) (tape_list t) in
-                    let new_t = Tp (data_p t) (s ++ [v] ++ (tail e)) in
-                      return $! new_t
+bracemap_r :: Int -> [Char] -> [Int] -> Data.Map.Map Int Int -> Data.Map.Map Int Int
+-- See an opening '['
+bracemap_r idx ('[':rest) (stack) m =
+  bracemap_r (idx + 1) rest (idx:stack) m
+-- See a "proper" ']' (has an opening '[')
+bracemap_r idx (']':rest) (prev:stack) m =
+  bracemap_r (idx + 1) rest (stack)
+  (Data.Map.union m (Data.Map.fromList [(idx, prev), (prev, idx)]))
+-- See an "improper" ']' (does not have an opening ']')
+-- We choose to ignore it (bad practice!), so this case only
+-- exists so we may override it some time in the future
+bracemap_r idx (']':rest) [] m =
+  bracemap_r (idx + 1) rest [] m
+bracemap_r idx (_:rest) stack m =
+  bracemap_r (idx + 1) rest stack m
+bracemap_r _ _ _ m =
+  m
